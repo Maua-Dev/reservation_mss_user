deb2839453175a92e8eea27031c22b41e6055884
lseixas <lseixas.iorio@gmail.com>
2024-10-24 22:22:13 -0300

    iac bala

--- iac/iac/dynamo_table.py
+++ iac/iac/dynamo_table.py
@@ -5,14 +5,14 @@ from aws_cdk import (
 from constructs import Construct
 
 
-class TemplateDynamoTable(Construct):
+class ReservationMssUserDynamoTable(Construct):
     table: dynamodb.Table
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         self.table = dynamodb.Table(
-            self, "TemplateDynamoTable",
+            self, "ReservationMssUserDynamoTable",
             partition_key=dynamodb.Attribute(
                 name="PK",
                 type=dynamodb.AttributeType.STRING
@@ -20,11 +20,7 @@ class TemplateDynamoTable(Construct):
             sort_key=dynamodb.Attribute(
                 name="SK",
                 type=dynamodb.AttributeType.STRING
-            ), 
+            ),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
             removal_policy=RemovalPolicy.DESTROY
         )
-        
-    
-
-
--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -44,56 +44,64 @@ class LambdaStack(Construct):
         return function
 
     def __init__(self, scope: Construct, api_gateway_resource: Resource, environment_variables: dict) -> None:
-        super().__init__(scope, "Template_Lambdas")
+        super().__init__(scope, "ReservationMssUser")
 
-        self.lambda_layer = lambda_.LayerVersion(self, "Template_Layer",
-                                                 code=lambda_.Code.from_asset("./lambda_layer_out_temp"),
+        self.lambda_layer = lambda_.LayerVersion(self, "ReservationMssUser_Layer",
+                                                 code=lambda_.Code.from_asset("./lambda_layer_ReservationMssUser"),
                                                  compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
                                                  )
 
         self.lambda_power_tools = lambda_.LayerVersion.from_layer_version_arn(self, "Lambda_Power_Tools",
                                                                               layer_version_arn="arn:aws:lambda:us-east-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:22")
 
-        # TODO -> Olha o método de create_lambda_api_gateway_integration, dê uma olhada no Function()
-        # TODO -> Crie a Lambda do Authorizer, passando o Path dele certinho.
-        # TODO -> Utilize o apigateway para criar o TokenAuthorizer
-
-        authorizer_lambda = lambda_.Function()
-        apigw.TokenAuthorizer(
-            self, "Authorizer",
-            handler=authorizer_lambda,
-            identity_source=apigw.IdentitySource.header("Authorization"),
-            authorizer_name="Authorizer",
-            results_cache_ttl=Duration.minutes(5)
+        authorizer_lambda = lambda_.Function(
+            self, "LambdaAuthorizerReservationMssUser",
+            code = lambda_.Code.from_asset("../src/functions"),
+            handler = "authorizer.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            layers = [self.lambda_layer, self.lambda_power_tools],
+            environment = environment_variables,
+            timeout=Duration.seconds(15)
         )
 
+        token_authorizer_lambda = apigw.TokenAuthorizer(
+            self, "TokenAuthorizerReservationMssUser",
+            handler=authorizer_lambda,
+            identity_source=apigw.IdentitySource.header("Authorization"),
+            authorizer_name="LambdaAuthorizerReservationMssUser",
+            results_cache_ttl=Duration.minutes(5)
+        )
 
         self.get_user_function = self.create_lambda_api_gateway_integration(
             module_name="get_user",
             method="GET",
             mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
+            environment_variables=environment_variables,
+            authorizer=token_authorizer_lambda
         )
 
         self.create_user_function = self.create_lambda_api_gateway_integration(
             module_name="create_user",
             method="POST",
             mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
+            environment_variables=environment_variables,
+            authorizer=token_authorizer_lambda
         )
 
         self.delete_user_function = self.create_lambda_api_gateway_integration(
             module_name="delete_user",
             method="POST",
             mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
+            environment_variables=environment_variables,
+            authorizer=token_authorizer_lambda
         )
 
         self.update_user_function = self.create_lambda_api_gateway_integration(
             module_name="update_user",
             method="POST",
             mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
+            environment_variables=environment_variables,
+            authorizer=token_authorizer_lambda
         )
 
         self.functions_that_need_dynamo_permissions = [self.get_user_function, self.create_user_function,
