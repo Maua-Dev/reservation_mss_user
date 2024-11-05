0246567b062982f824b3206e2d4ce35a15afa202
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-11-05 15:44:00 -0300

    remove observality

--- src/modules/get_user/app/get_user_presenter.py
+++ src/modules/get_user/app/get_user_presenter.py
@@ -4,7 +4,7 @@ from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 
 
-repo =  Environments().get_user_repo()
+repo =  Environments.get_user_repo()()
 usecase = GetUserUseCase(repo=repo)
 controller = GetUserController(usecase=usecase)
 
--- tests/modules/get_user/app/test_get_user_presenter.py
+++ tests/modules/get_user/app/test_get_user_presenter.py
@@ -1,66 +1,66 @@
-# import os
-# os.environ['STAGE'] = 'TEST'
+import os
+os.environ['STAGE'] = 'TEST'
 
-# import json
-# from src.modules.get_user.app.get_user_presenter import lambda_handler
-# from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+import json
+from src.modules.get_user.app.get_user_presenter import lambda_handler
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
 
-# first_user = UserRepositoryMock().users_list[0]
-# third_user  = UserRepositoryMock().users_list[2]
+first_user = UserRepositoryMock().users_list[0]
+third_user  = UserRepositoryMock().users_list[2]
 
-# class Test_GetUserPresenter:
+class Test_GetUserPresenter:
 
-#     def test_get_user_presenter(self):
-#         event = {
-#             "version": "2.0",
-#             "routeKey": "$default",
-#             "rawPath": "/my/path",
-#             "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
-#             "cookies": [
-#                 "cookie1",
-#                 "cookie2"
-#             ],
-#             "headers": {
-#                 "header1": "value1",
-#                 "header2": "value1,value2"
-#             },
-#             "requestContext": {
-#                 "accountId": "123456789012",
-#                 "apiId": "<urlid>",
-#                 "authentication": None,
-#                 "authorizer": {
-#                     "claims":
-#                     {
-#                         "user_id": first_user.user_id,
-#                         "email": first_user.email,
-#                         "name": first_user.name,
-#                     }
-#                 },
-#                 "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
-#                 "domainPrefix": "<url-id>",
-#                 "external_interfaces": {
-#                     "method": "POST",
-#                     "path": "/my/path",
-#                     "protocol": "HTTP/1.1",
-#                     "sourceIp": "123.123.123.123",
-#                     "userAgent": "agent"
-#                 },
-#                 "requestId": "id",
-#                 "routeKey": "$default",
-#                 "stage": "$default",
-#                 "time": "12/Mar/2020:19:03:58 +0000",
-#                 "timeEpoch": 1583348638390
-#             },
-#             "body": "Hello from client!",
-#             "pathParameters": None,
-#             "isBase64Encoded": None,
-#             "stageVariables": None
-#         }
+    def test_get_user_presenter(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "claims":
+                    {
+                        "user_id": first_user.user_id,
+                        "email": first_user.email,
+                        "name": first_user.name,
+                    }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": "Hello from client!",
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
 
-#         response = lambda_handler(event, None)
-#         print("Response Body:", response.get("body"))
+        response = lambda_handler(event, None)
+        print("Response Body:", response.get("body"))
 
-#         assert response["statusCode"] == 200
+        assert response["statusCode"] == 200
 
 
 
