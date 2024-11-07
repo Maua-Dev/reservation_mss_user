6f5f5f839fb2bce486619ea52f48f6656229fe59
gioalbuquerque <gio.oliver.albuquerque@gmail.com>
2024-11-06 22:07:34 -0300

    correção viewmodel

--- src/modules/update_user/app/update_user_viewmodel.py
+++ src/modules/update_user/app/update_user_viewmodel.py
@@ -4,30 +4,20 @@ from src.shared.domain.enums.role_enum import ROLE
 
 
 class UserViewModel:
-
-    name: str
-    email: str
     user_id: str
-    ra: Optional[str] = None
     role: ROLE
     confirm_user: bool
 
     def __init__(self, user: User):
         self.user_id = user.user_id
-        self.name = user.name
-        self.email = user.email
-        self.ra = user.ra
         self.role = user.role
         self.confirm_user = user.confirm_user
 
     def to_dict(self) -> dict:
         return {
             'user_id': self.user_id,
-            'name': self.name,
-            'email': self.email,
-            'ra': self.ra,
             'role': self.role.value,
-            'confirm_user': self.confirm_user,
+            'confirm_user': self.confirm_user
         }
 
 class UpdateUserViewModel:
