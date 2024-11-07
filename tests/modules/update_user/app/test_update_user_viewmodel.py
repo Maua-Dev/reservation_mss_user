a2aaab40aa6e0976cc12e08e9753e08bbee8b55e
gioalbuquerque <gio.oliver.albuquerque@gmail.com>
2024-11-06 22:40:14 -0300

    teste viewmodel

--- tests/modules/update_user/app/test_update_user_viewmodel.py
+++ tests/modules/update_user/app/test_update_user_viewmodel.py
@@ -1,21 +1,20 @@
-import pytest
 from src.shared.domain.enums.role_enum import ROLE
+from src.shared.domain.entities.user import User
 from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
 from src.modules.update_user.app.update_user_viewmodel import UpdateUserViewModel
-
 class TestUpdateUserViewModel:
-    def test_update_user_viewmodel(self):
-        repo = UserRepositoryMock()
-        user = repo.get_user("93bc6ada-c0d1-8754-26ab-e17414c48ae7") 
 
-        viewmodel = UpdateUserViewModel(user)
+    def test_update_user_viewmodel(self):
+        user_id = "93bc6ada-c0d1-8754-26ab-e17414c48ae7"
+        repo = UserRepositoryMock()
+
+        user = repo.get_user(user_id)
+
+        viewmodel = UpdateUserViewModel(user=user)
 
         expected = {
             'updated_user': {
                 'user_id': user.user_id,
-                'name': user.name,
-                'email': user.email,
-                'ra': user.ra,
                 'role': user.role.value,
                 'confirm_user': user.confirm_user,
             },
