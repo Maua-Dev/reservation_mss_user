84241856bccf2f442b59ade9263f331da934c006
gioalbuquerque <gio.oliver.albuquerque@gmail.com>
2024-11-06 22:39:25 -0300

    nova correção do controller

--- src/modules/update_user/app/update_user_controller.py
+++ src/modules/update_user/app/update_user_controller.py
@@ -1,4 +1,5 @@
 from typing import Any
+import json
 from .update_user_usecase import UpdateUserUsecase
 from .update_user_viewmodel import UpdateUserViewModel
 from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
@@ -16,28 +17,40 @@ class UpdateUserController:
 
     def __call__(self, request: IRequest):
         try:
-            user_id = request.data.get('user_id')
+
+            if isinstance(request.data.get('user_from_authorizer'), str):
+                user_to_update = json.loads(request.data.get('user_from_authorizer'))
+
+            else:
+                user_to_update = request.data.get('user_from_authorizer')
+
+ 
+            user_id = user_to_update.get('user_id')
             if user_id is None:
                 raise MissingParameters('user_id')
 
             if not isinstance(user_id, str):
                 raise WrongTypeParameter('user_id', 'str', type(user_id).__name__)
 
-            confirm_user = request.data.get('confirm_user')
+            confirm_user = user_to_update.get('confirm_user')
             if confirm_user is None:
                 raise MissingParameters('confirm_user')
 
             if not isinstance(confirm_user, bool):
                 raise WrongTypeParameter('confirm_user', 'bool', type(confirm_user).__name__)
+            
 
-            role_str = request.data.get('role')
+            role_str = user_to_update.get('role')
             if role_str is None:
                 raise MissingParameters('role')
 
-            if not isinstance(role_str, str) or role_str not in [role.value for role in ROLE]:
+            if not isinstance(role_str, str):
                 raise EntityParameterError('Field role is not valid')
+            for role in ROLE:
+                if role_str == role.value:
+                    new_role = role
 
-            updated_user = self.usecase(user_id=user_id, confirm_user=confirm_user, role=ROLE[role_str])
+            updated_user = self.usecase(user_id=user_id, confirm_user=confirm_user, role=new_role)
 
             viewmodel = UpdateUserViewModel(updated_user)
             return OK(viewmodel.to_dict())
