18d084b31752329851607c2bf0c52fec00e4347b
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-11-07 22:17:26 -0300

    Atribuindo a instância da pilha a uma variável para melhor legibilidade

--- iac/app.py
+++ iac/app.py
@@ -40,7 +40,7 @@ tags = {
     'owner': 'DevCommunity'
 }
 
-Stack(app, stack_name=stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
+stack = Stack(app, stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
 
 
 app.synth()
