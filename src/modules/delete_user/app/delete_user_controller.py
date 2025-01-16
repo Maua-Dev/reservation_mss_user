67249d1e74b4a0b24ba1e7f03f88d11a41d4af1a
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2025-01-15 23:45:45 -0300

    fix: controller

--- src/modules/delete_user/app/delete_user_controller.py
+++ src/modules/delete_user/app/delete_user_controller.py
@@ -1,8 +1,6 @@
-import json
-
 from src.modules.delete_user.app.delete_user_usecase import DeleteUserUsecase
 from src.modules.delete_user.app.delete_user_viewmodel import DeleteUserViewmodel
-from src.shared.helpers.errors.controller_errors import MissingParameters, Denied
+from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
 from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
 from src.shared.helpers.errors.usecase_errors import DuplicatedItem
@@ -16,21 +14,29 @@ class DeleteUserController:
         
     def __call__(self, request: IRequest) -> IResponse:
         try:
-            if request.data.get('user_id') is None:
+            user_id = request.data.get('user_id')
+
+            if user_id is None:
                 raise MissingParameters('user_id')
             
-            user = self.usecase(user_id=request.data.get('user_id'))
+            if not isinstance(user_id, str):
+                raise WrongTypeParameter('user_id', expected_type='str')
+            
+            user = self.usecase(user_id=user_id)
             viewmodel = DeleteUserViewmodel(user)
             
             return OK(viewmodel.to_dict())
-        
-        except DuplicatedItem as err:
-            return BadRequest(body=err.message)
 
+        except MissingParameters as err:
+            return BadRequest(body=f"Field user_id is missing")
+
+        except WrongTypeParameter as err:
+            return BadRequest(body=f"Field {err.parameter} should be of type {err.expected_type}")
+        
         except EntityError as err:
             return BadRequest(body=err.message)
 
-        except Denied as err:
+        except NoItemsFound as err:
             return NotFound(body=err.message)
 
         except Exception as err:
