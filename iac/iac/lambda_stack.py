1512853f6e4bbbaa54c7d4d9390389a66e9f80c0
lseixas <lseixas.iorio@gmail.com>
2025-05-13 19:10:49 -0300

    added dynamo permission to auth user lambda

--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -143,4 +143,4 @@ class LambdaStack(Construct):
         self.functions_that_need_dynamo_permissions = [self.get_user_function, self.create_user_function,
                                                        self.get_all_users_function,
                                                        self.delete_user_function, self.update_user_function,
-                                                       authorizer_lambda]
+                                                       authorizer_lambda, self.auth_user_function]
