84f109b5e164b5da2cbe88f3b83140693ae34e3c
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-07-20 13:27:31 -0300

    added admin logic to create and auth user (create is indeed unused), added tests

--- tests/modules/auth_user/app/test_auth_user_presenter.py
+++ tests/modules/auth_user/app/test_auth_user_presenter.py
@@ -54,6 +54,57 @@ class TestAuthUserPresenter:
         response = lambda_handler(event, None)
 
         assert response["statusCode"] == 200
+        
+    def test_auth_user_presenter_admin(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "user": json.dumps({
+                        "id": "93bc6ada-c0d1-7054-26ab-e17414c48fe5",
+                        "mail": "ceaf@maua.br",
+                        "displayName": "CEAF",
+                    })
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": "Hello from client!",
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+
+        response = lambda_handler(event, None)
+
+        assert response["statusCode"] == 200
+        assert "ADMIN" in response["body"]
 
     def test_auth_user_deny_from_authorizer(self):
         event = {
