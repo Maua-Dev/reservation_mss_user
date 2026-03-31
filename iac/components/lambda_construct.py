d547326855b4cb39bf69fa94924095cf44db4a6c
Leo Iorio <lseixas.iorio@gmail.com>
2026-03-31 16:22:15 -0300

    fixing region in power tools

--- iac/components/lambda_construct.py
+++ iac/components/lambda_construct.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     aws_lambda as lambda_,
     Duration,
     aws_apigateway as apigw,
+    Aws
 )
 from constructs import Construct
 from aws_cdk.aws_apigateway import Resource, LambdaIntegration, TokenAuthorizer
@@ -74,7 +75,7 @@ class LambdaConstruct(Construct):
         self.lambda_power_tools = lambda_.LayerVersion.from_layer_version_arn(
             self, 
             id=f"Lambda_Power_Tools-{stack_name}-{stage}",
-            layer_version_arn=f"arn:aws:lambda:{self.lambda_region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-python313-x86_64:30"
+            layer_version_arn=f"arn:aws:lambda:{Aws.REGION}:017000801446:layer:AWSLambdaPowertoolsPythonV3-python313-x86_64:30"
         )
 
         authorizer_lambda = lambda_.Function(
